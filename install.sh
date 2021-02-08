d5a902e6206db3aef87fa81d9ddaf4df01adf706
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2021-02-08 18:03:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 pkg update -y
 pkg upgrade -y
-pkg install curl
-pkg install php -y
-pkg install figlet -y
 pip install --upgrade pip
 clear
 echo
@@ -16,12 +13,12 @@ curl https://raw.githubusercontent.com/rixon-cochi/rixon-cochi/main/fbvideo.py >
 clear
 curl https://raw.githubusercontent.com/rixon-cochi/rixon-cochi/main/requirements.txt >> requirements.txt
 clear
-echo ' code developer TECH COCHI '
+echo " code developer TECH COCHI "
 sleep 6
 xdg-open https://www.youtube.com/c/TECHCOCHI2
 echo 
 sleep 6
-echo ' now enter bash run.sh '
+echo " now enter bash run.sh "
 
 
 
